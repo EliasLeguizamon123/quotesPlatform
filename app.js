21de8faf591cdda32a48d97c70cdeb7121d5d875
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2021-05-04 14:20:47 -0300

    I finish the backend, next i add frontend

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 require('dotenv').config({ path: './.env'});
 
@@ -11,15 +10,21 @@ var quotesRouter = require('./routes/quotes');
 
 //Routes
 app.use(require('./routes/quotes.js'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use('/quotes', quotesRouter);
-
-
-//Middleware
-app.use(bodyParser.json());
 
 //Starting server
 app.listen(process.env.PORT, () => {
     console.log('Listen on port: ' + process.env.PORT);
+});
+
+// Get random quote
+router.get('/random', async (req, res) => {
+	const count = await Quote.countDocuments();
+	const random = Math.floor(Math.random() * count);
+	const randomQuote = await Quote.findOne().skip(random);
+	res.json(randomQuote);
 });
 
 module.exports = app;
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,19 +1,37 @@
-const express = require('express')
+const express = require('express');
+const { findOne } = require('../models/quotes');
 const router = express.Router();
 const quote = require('../models/quotes');
-//get all routes
+
+//get all quotes
 router.get('/', async (req, res) => {
-    // const quotes = await quote.find()
-    // res.json(quotes);
-    res.send('Ok its work')
+    const quotes = await quote.find()
+    res.json(quotes);
 });
 
 //New quotes
 router.post('/new', async (req, res) => {
-    // const newQuote = new quote(req.body)
-    // const savedQuote = await newQuote.save();
-    // res.json(savedQuote);
-    res.send('its work')
+    const newQuote = new quote(req.body)
+    const savedQuote = await newQuote.save();
+    res.json(savedQuote);
+});
+
+//find a Quote by ID
+router.get('/get/:id', async (req, res) => {
+    const findQuote = await quote.findById({_id: req.params.id})
+    res.json(findQuote)
+});
+
+//Delete quote
+router.get('/delete/:id', async (req, res) => {
+    const eraseQuote = await quote.findByIdAndDelete({_id: req.params.id});
+    res.json(eraseQuote)
+});
+
+//Update quote
+router.patch('/update/:id', async (req, res) =>{
+    const updateQuote = await quote.updateOne({_id: req.params.id}, {$set: req.body})
+    res.send(updateQuote)
 });
 
 module.exports = router;
